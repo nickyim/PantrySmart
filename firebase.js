c27b5656afb2abd148b44d9f341b7c3fb583c3df
nickyim <nicholasjyim@gmail.com>
2024-08-04 22:00:34 -0400

    Initial

--- firebase.js
+++ firebase.js
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
+import dotenv from "dotenv";
+
+dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyALdgCCih2jhuB8GwYVPjFMxkOuEjko_1U",
-  authDomain: "pantryapp-5bd30.firebaseapp.com",
-  projectId: "pantryapp-5bd30",
-  storageBucket: "pantryapp-5bd30.appspot.com",
-  messagingSenderId: "219361300436",
-  appId: "1:219361300436:web:66b0c78c5cb7434a9d9041"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
